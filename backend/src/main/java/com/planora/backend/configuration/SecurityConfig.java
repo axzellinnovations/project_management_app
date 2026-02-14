ebc92cd70b386c77940cab02e5e3ac6d680c717e
Suthankan Balenthiran <Suthankanbala2019@gmail.com>
2026-02-14 11:09:33 +0530

    user api changed, email set to lowercase.

--- backend/src/main/java/com/planora/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/planora/backend/configuration/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         http
                 .csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request ->request
-                        .requestMatchers("/register", "/login", "/reg/verify", "/resend", "/forgot", "/reset")
+                        .requestMatchers("/api/auth/**")
                         .permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
